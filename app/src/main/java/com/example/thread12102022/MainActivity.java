450f13c21ad55715a05def14ce9f62a1f3d0c1ad
Phat <phatdroid94@gmail.com>
2022-11-30 18:31:00 +0700

    handle logic

--- app/src/main/java/com/example/thread12102022/MainActivity.java
+++ app/src/main/java/com/example/thread12102022/MainActivity.java
@@ -8,20 +8,26 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     int a, b, c;
-
+    MyFlag myFlag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         a = b = c = 0;
+        myFlag = new MyFlag(0);
 
         Thread threadA = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 50; i++) {
-                    a = i;
-                    Log.d("BBB", "A: " + a);
+                synchronized (myFlag) {
+                    for (int i = 0; i < 50; i++) {
+                        if (myFlag.count == 0) {
+                            a = i;
+                            Log.d("BBB", "A: " + a);
+                            myFlag.count = 1;
+                        }
+                    }
                 }
             }
         });
@@ -30,9 +36,14 @@ public class MainActivity extends AppCompatActivity {
         Thread threadB = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 50; i++) {
-                    b = i;
-                    Log.d("BBB", "B: " + b);
+                synchronized (myFlag) {
+                    for (int i = 0; i < 50; i++) {
+                        if (myFlag.count == 1) {
+                            b = i;
+                            Log.d("BBB", "B: " + b);
+                            myFlag.count = 2;
+                        }
+                    }
                 }
             }
         });
@@ -40,9 +51,14 @@ public class MainActivity extends AppCompatActivity {
         Thread threadC = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (int i = 0; i < 50; i++) {
-                    c = a + b;
-                    Log.d("BBB", "C: " + c);
+                synchronized (myFlag) {
+                    for (int i = 0; i < 50; i++) {
+                       if (myFlag.count == 2) {
+                           c = a + b;
+                           Log.d("BBB", "C: " + c);
+                           myFlag.count = 0;
+                       }
+                    }
                 }
             }
         });
---
+++ app/src/main/java/com/example/thread12102022/MyFlag.java
@@ -0,0 +1,12 @@
+package com.example.thread12102022;
+
+/**
+ * Created by pphat on 11/30/2022.
+ */
+public class MyFlag {
+    int count;
+
+    public MyFlag(int count) {
+        this.count = count;
+    }
+}
